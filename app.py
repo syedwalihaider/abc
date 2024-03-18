989a7f934e308d16a2e6b773edcd19c318f32248
wali <wali.h446@gmail.com>
2024-03-18 16:12:15 +0530

    modify app.py

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ col1, col2 = st.columns(2)
 with col1:
     st.image('unnamed.jpg')
 with col2:
-    st.write("kdfnvkjengqnejvkjnevj uehp9eyhibei9uvh8w9huhybhi ohbih")
+    st.write("sjfngebhgrhebgbekjrngiehg ejrngiehdrigherig kejrhgkejbdgi9uvh8w9huhybhi ohbih")
