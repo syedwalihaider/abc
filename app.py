25a679646608e93409fee5d28c83a82900e6bd85
wali <wali.h446@gmail.com>
2024-03-19 11:08:50 +0530

    Add some course and heading

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ with col1:
     st.image('unnamed.jpg')
 with col2:
     st.write("sjfngebhgrhebgbekjrngiehg ejrngiehdrigherig kejrhgkejbdgi9uvh8w9huhybhi ohbih")
+
+
+st.header("CampusX online Course")
+st.subheader('DSMP')
